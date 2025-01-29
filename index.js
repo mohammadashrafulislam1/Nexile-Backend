ea4619cb15ca9cec6669b03717f477e604ecdf32
Md Ashraful Islam <90489175+mohammadashrafulislam1@users.noreply.github.com>
2025-01-28 19:43:53 -0800

    Update index.js

--- index.js
+++ index.js
@@ -25,8 +25,24 @@ configDotenv();
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Allow multiple origins
+const allowedOrigins = [
+  "https://nexile-dashboard.vercel.app",
+  "http://localhost:5173"
+];
 
-app.use(cors())
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Allow cookies & authentication headers
+  })
+);
 
 // Middleware to parse JSON
 app.use(express.json());
